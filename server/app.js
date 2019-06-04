233f6b891f63bd0821213f33e90839f97de465ab
Daniel Pan <daniel.pan@inmobi.com>
2019-06-04 13:59:11 -0700

    Separated app and sever

---
+++ server/app.js
@@ -0,0 +1,42 @@
+/* eslint no-console: 0 */
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { ApolloServer } from 'apollo-server-express';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import models from './models';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+
+// import { customScalarTypeDefs, customScalarResolvers } from './customScalars'
+
+// Force close connection, sometimes it persists
+// db.sequelize.close()
+
+if (process.env.NODE_ENV === 'development') {
+  // Sync the database models
+  models.sequelize.sync({
+    force: true,
+  });
+}
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+
+// console.log(customScalarTypeDefs)
+// console.log(customScalarResolvers)
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { models },
+});
+
+server.applyMiddleware({ app });
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,5 @@
-/* eslint no-console: 0 */
+import app from './app';
 
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { ApolloServer } from 'apollo-server-express';
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-import models from './models';
-// import { customScalarTypeDefs, customScalarResolvers } from './customScalars'
+const PORT = process.env.PORT || 4000;
 
-// Force close connection, sometimes it persists
-// db.sequelize.close()
-// Sync the database models
-models.sequelize.sync({
-  force: true,
-});
-
-const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
-const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
-
-// console.log(customScalarTypeDefs)
-// console.log(customScalarResolvers)
-
-const runServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: { models },
-  });
-
-  const app = express();
-  app.use(cors());
-  server.applyMiddleware({ app });
-  const PORT = process.env.PORT || 4000;
-
-  app.listen(PORT, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
-};
-
-try {
-  runServer();
-} catch (err) {
-  console.error(err);
-}
+app.listen(PORT, () => console.log(`🚀 Server ready at http://localhost:${PORT}`));
