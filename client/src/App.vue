aa0d9751cfdf018f5b94785bda82de3e789ec9da
Daniel Pan <daniel.pan@inmobi.com>
2019-06-04 15:33:01 -0700

    vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+fff
+    <layout></layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from './Layout.vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Layout
   }
+
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import '@fortawesome/fontawesome-free/css/all.css'; // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App),
