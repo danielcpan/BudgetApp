56367c51ef2d4b4f16f36b2848b994d47b019b6c
Daniel Pan <daniel.pan@inmobi.com>
2019-06-17 07:33:47 -0700

    Attempting to build to heroku

--- server/app.js
+++ server/app.js
@@ -48,10 +48,12 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
+
+console.log(__dirname)
 
 module.exports = app;
