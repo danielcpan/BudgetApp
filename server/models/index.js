481cc16d22b4c82923e8899989aae8b80914a790
Daniel Pan <daniel.pan@inmobi.com>
2019-06-17 10:31:12 -0700

    Test again...

--- server/models/index.js
+++ server/models/index.js
@@ -14,11 +14,11 @@ var sequelize = null;
 if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
+    dialect: 'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  true //false
+    dialectOptions: {
+        ssl: true
+    }
   })
 } else {
   // the application is executed on the local machine ... use mysql
