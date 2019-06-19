a605dbb18e04df81f583d2e61fa85ad7658d8266
Daniel Pan <daniel.pan@inmobi.com>
2019-06-19 00:32:28 -0700

    AuthGuard

--- server/resolvers/expense.resolver.js
+++ server/resolvers/expense.resolver.js
@@ -1,4 +1,6 @@
 /* eslint no-unused-vars: 0 */
+const requiresAuth = require('../helpers/permissions');
+const { auth } = require('../helpers/permissions');
 
 module.exports = {
   Expense: {
@@ -6,23 +8,23 @@ module.exports = {
     category: (parent, args, { models }, info) => parent.getCategory(),
   },
   Query: {
-    expense: (parent, { id }, { models }, info) => (
+    expense: auth((parent, { id }, { models }, info) => (
       models.Expense.findByPk(id)
-    ),
-    expenses: (parent, args, { models }, info) => (
+    )),
+    expenses: auth((parent, args, { models }, info) => (
       models.Expense.findAll({ where: args })
-    ),
+    )),
   },
   Mutation: {
-    createExpense: (parent, { input }, { models }, info) => (
+    createExpense: auth((parent, { input }, { models }, info) => (
       models.Expense.create(input)
-    ),
-    updateExpense: async (parent, { input }, { models }, info) => {
+    )),
+    updateExpense: auth(async (parent, { input }, { models }, info) => {
       await models.Expense.update(input, { where: { id: input.id } });
       return models.Expense.findByPk(input.id);
-    },
-    deleteExpense: (parent, { id }, { models }, info) => (
+    }),
+    deleteExpense: auth((parent, { id }, { models }, info) => (
       models.Expense.destroy({ where: { id } })
-    ),
+    )),
   },
 };
