2f33538b86908e166a6908064858a5b6f3b3a5d5
Daniel Pan <daniel.pan@inmobi.com>
2019-06-23 20:41:53 -0700

    Test

--- client/src/apolloProvider.js
+++ client/src/apolloProvider.js
@@ -1,18 +1,3 @@
-// import Vue from 'vue';
-// import VueApollo from 'vue-apollo';
-// import ApolloClient from 'apollo-boost';
-
-// Vue.use(VueApollo);
-
-// export const apolloClient = new ApolloClient({
-//   // uri: 'https://budget-app-daniel-pan.herokuapp.com/graphql',
-//   uri: 'http://localhost:5000/graphql',
-// });
-
-// export default new VueApollo({
-//   defaultClient: apolloClient,
-// });
-
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
 import { ApolloClient } from 'apollo-client';
