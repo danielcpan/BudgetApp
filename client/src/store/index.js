e1bb5109c8110d38c0d145923a0d8ac39413859c
Daniel Pan <danielcpan@gmail.com>
2019-07-24 15:59:20 -0700

    Automatic module registration implemented

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,18 +2,10 @@
 
 import Vue from 'vue';
 import Vuex from 'vuex';
-import SnackbarModule from './modules/snackbar.module';
-import UsersModule from './modules/users.module';
-import CategoriesModule from './modules/categories.module';
-import ExpensesModule from './modules/expenses.module';
+import modules from './modules';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    users: UsersModule,
-    categories: CategoriesModule,
-    expenses: ExpensesModule,
-    snackbar: SnackbarModule,
-  },
+  modules
 });
