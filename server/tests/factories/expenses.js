0439ad42af90494555eaf4928d2de4576ff317e9
Daniel Pan <danielcpan@gmail.com>
2019-07-26 15:44:50 -0700

    Fix test data bug

--- server/tests/factories/expenses.js
+++ server/tests/factories/expenses.js
@@ -7,7 +7,7 @@ module.exports = factory.define('Expense', models.Expense, (buildOptions = {}) =
   const attrs = {
     cost: () => faker.finance.amount(0, 200),
     note: () => faker.lorem.sentence(),
-    date: () => format(faker.date.recent(30), 'YYYY-MM-DD').toISOString(),
+    date: () => format(faker.date.recent(30).toISOString(), 'YYYY-MM-DD'),
     categoryId: factory.assoc('Category', 'id'),
     userId: factory.assoc('User', 'id'),
   };
